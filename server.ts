d9404a7c399b9795d77c809085dbdf8e261d0bfa
Zakaria MOUCHTATI <z.mouchtati@gmail.com>
2025-07-12 01:05:37 +0100

    deploy

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import bodyParser from "body-parser";
 import { getVectoreStore } from "./utils/vector";
 import { genrateAnswer } from "./utils/generate";
 import { sendMessage } from "./utils/message";
+import { mainFunction } from "./utils/llm";
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -12,11 +13,10 @@ let vectorStore: any;
 
 app.post("/uir-chat-bot", async (req, res) => {
   const message = req.body;
-  const answer = await genrateAnswer(message.Body, message.From);
-
+  const answer = await mainFunction(message.Body, message.From);
   await sendMessage(message.From, answer);
 
-  res.send({ message: message.Body, answer: answer });
+  res.send({ message: message.Body, answer });
 });
 
 const PORT = 7001;
